502d27033f85db947788e6514586beb36373dd4e
Ruslan <zautsev.ruslan@yanswx.ru>
2023-04-11 09:52:09 +0300

    fin

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -10,30 +10,34 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
+
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
+
     @Transactional
     @Override
     public void add(User user) {
         userDao.add(user);
     }
-    @Transactional
+
     @Override
     public List<User> getAllUser() {
         return userDao.getAllUser();
     }
+
     @Transactional
     @Override
     public void update(User user) {
         userDao.update(user);
     }
+
     @Transactional
     @Override
     public void delete(User user) {
         userDao.delete(user);
     }
-    @Transactional
+
     @Override
     public User deleteUserById(Long id) {
         return userDao.deleteUserById(id);
